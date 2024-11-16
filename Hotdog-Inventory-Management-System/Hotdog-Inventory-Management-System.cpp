be3c0ecc7838e9cbd0eabb8ab5ed1b77da272827
JangoVhett <sean2h2armstrong@gmail.com>
2024-11-16 12:04:41 -0500

    expired report reports correct info

--- Hotdog-Inventory-Management-System/Hotdog-Inventory-Management-System.cpp
+++ Hotdog-Inventory-Management-System/Hotdog-Inventory-Management-System.cpp
@@ -7,7 +7,9 @@
 #include <vector>
 #include <algorithm> //for transform
 #include "InventoryObj.cpp"
-#include "BaseReport.h"
+//#include "BaseReport.h"
+#include "ExpiredReport.h"
+
 
 using namespace std;
 
@@ -67,12 +69,15 @@ void displayAllItems(const vector<InventoryItems>& inventory) {
         return;
     }
     BaseReport foo;
+    ExpiredReport bar;
 
     for (const auto& item : inventory) {
         foo.addData(item);
+        bar.addData(item);
     }
 
-    foo.generateReport();
+    //foo.generateReport();
+    bar.generateReport();
 }
 
 void enterData(vector<InventoryItems>& inventory) {
